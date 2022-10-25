4d90b511ca186fdeb787791a1856d7232aad45e1
Yosuke Adachi <yosuke@humn.ai>
2022-10-25 12:34:56 +0100

    fix: Update lambda script and open outbound port on ALB

--- auth.tf
+++ auth.tf
@@ -26,6 +26,7 @@ resource "aws_lb_listener_rule" "https" {
 
 # api access via api-gateway rules
 resource "aws_lb_listener_rule" "api" {
+  priority     = 2
   listener_arn = aws_lb_listener.http.arn
 
   condition {
@@ -49,6 +50,7 @@ resource "aws_lb_listener_rule" "api" {
 
 
 resource "aws_lb_listener_rule" "http" {
+  priority     = 3
   listener_arn = aws_lb_listener.http.arn
 
   condition {
@@ -76,11 +78,11 @@ resource "aws_lb_listener_rule" "http" {
 }
 
 resource "aws_security_group_rule" "lb_egress_idp" {
-  description       = "Open 443 outbound for idp access"
+  description       = "Open outbound for all"
   type              = "egress"
-  from_port         = "443"
-  to_port           = "443"
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.lb.id
 }
